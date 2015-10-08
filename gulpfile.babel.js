88e48defa246b7b36d866273cc439d5376431127
zallek <nicolas.fortin37@gmail.com>
2015-10-08 12:16:49 +0200

    Fix npm link script

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,46 +1,21 @@
+import argv from 'argv';
 import gulp from 'gulp';
 import bump from 'gulp-bump';
-import eslint from 'gulp-eslint';
 import shell from 'gulp-shell';
-import runSequence from 'run-sequence';
 import strftime from 'strftime';
 
 import webpackBuild from './webpack/build';
 
 
-const runEslint = () => {
-  return gulp.src([
-    '**/*.js',
-  ])
-  .pipe(eslint())
-  .pipe(eslint.format());
-};
-
-gulp.task('set-ci-environment', () => {
-  process.env.CONTINUOUS_INTEGRATION = true; // eslint-disable-line no-undef
-});
-
 gulp.task('build', webpackBuild);
 
-gulp.task('eslint', () => {
-  return runEslint();
-});
-
-gulp.task('eslint-ci', () => {
-  return runEslint().pipe(eslint.failAfterError());
-});
-
-gulp.task('test-ci', (done) => {
-  runSequence('set-ci-environment', 'eslint-ci', 'build', done);
-});
-
-gulp.task('bump-version', function() {
+gulp.task('bump-version', () => {
   return gulp.src(['package.json'])
     .pipe(bump(argv))
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('release', ['dist', 'bump-version'], function() {
+gulp.task('release', ['dist', 'bump-version'], () => {
   return gulp.src('')
     .pipe(shell([
       'git add -u', // Add modified files (package.json, bower.json, ...)
