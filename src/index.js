882fbacfbe58e050d934d2d43ae3badfb5e7605f
Nicolas Fortin <nicolas.fortin37@gmail.com>
2015-10-13 15:32:29 +0200

    Add middlewares to exported modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import applyMiddleware from './applyMiddleware';
 import apiErrorMiddleware from './middlewares/apiErrorMiddleware';
+import batchMiddleware from './middlewares/batchMiddleware';
+import invalidateAnalysisMiddleware from './middlewares/invalidateAnalysisMiddleware';
 import lscacheMiddleware from './middlewares/lscacheMiddleware';
 
 const middlewares = {
   apiErrorMiddleware,
+  batchMiddleware,
+  invalidateAnalysisMiddleware,
   lscacheMiddleware,
 };
 
